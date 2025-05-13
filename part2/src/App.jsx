9a88fb826f8bd623b43622b6dc76d02f0cf7a42e
Carlos Q <carlos.quezada.dev@gmail.com>
2025-05-13 17:42:52 -0500

    automatico

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -56,7 +56,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>Notes</h1>
+      <h1>Notes con autoactualizacion</h1>
       <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
